f57a8b55b85691fc328404bd0d3288348c30b093
Tim02023 <rooah2002k@gmail.com>
2025-11-03 13:06:14 +0300

    15-register-user

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,4 +1,3 @@
-import { UserIcon } from 'lucide-react'
 import Link from 'next/link'
 import CartButton from './cart-button'
 import UserButton from './user-button'
@@ -6,18 +5,11 @@ import UserButton from './user-button'
 export default function Menu() {
   return (
     <div className="flex justify-end">
-      <nav className="flex items-center gap-3 w-full">
+      <nav className="flex items-center gap-3 w-full mr-5">
         <Link href="/sign-in" className="flex items-center header-button">
           <UserButton />
         </Link>
-        <Link href="/cart" className="header-button">
-          <UserIcon className="h-7 w-7 text-center mx-auto" />
-          <span className="font-bold text-sm">Sign in</span>
-        </Link>
-
-        <nav className="flex gap-3 w-full">
-          <CartButton />
-        </nav>
+         <CartButton />
       </nav>
     </div>
   )
